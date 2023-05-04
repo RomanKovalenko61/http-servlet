9e12c78f239cd10def0e0444b29f121acdc6044f
kb3-3 <astek14@mail.ru>
2023-05-04 10:18:27 +0300

    request parameters

--- src/main/java/com/example/servlet/FirstServlet.java
+++ src/main/java/com/example/servlet/FirstServlet.java
@@ -12,16 +12,26 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        var headerNames = req.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            var header = headerNames.nextElement();
-            System.out.println(req.getHeader(header));
-        }
+        var paramValue = req.getParameter("param");
+        var parameterMap = req.getParameterMap();
+        System.out.println();
+
+//        var headerNames = req.getHeaderNames();
+//        while (headerNames.hasMoreElements()) {
+//            var header = headerNames.nextElement();
+//            System.out.println(req.getHeader(header));
+//        }
         resp.setContentType("text/html; charset=UTF-8");
         resp.setHeader("token", "12345");
 //         resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
         try (var writer = resp.getWriter()) {
             writer.write("<h1>Привет с первого сервлета</h1>");
         }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var parameterMap = req.getParameterMap();
+        System.out.println(parameterMap);
     }
 }
