bf5af8a8ff4a90c043351c3fbb0bfd78abcbb811
kb3-3 <astek14@mail.ru>
2023-05-04 11:12:51 +0300

    download file from server

---
+++ src/main/java/com/example/servlet/DownloadServlet.java
@@ -0,0 +1,27 @@
+package com.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet("/download")
+public class DownloadServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("Content-Disposition", "attachment; filename=\"filename.txt\"");
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+//        Files.readAllBytes(Path.of("resources", "first.json"));
+        try (var outputStream = resp.getOutputStream();
+             var stream =
+                     DownloadServlet.class.getClassLoader().getResourceAsStream("first.json")) {
+            outputStream.write(stream.readAllBytes());
+        }
+    }
+}
