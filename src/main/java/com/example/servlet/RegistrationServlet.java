2e8c05dc29bca8577465753fa7794de4859a4b5a
RomanKovalenko61 <astek14@mail.ru>
2023-05-05 22:08:16 +0300

    Html forms

---
+++ src/main/java/com/example/servlet/RegistrationServlet.java
@@ -0,0 +1,31 @@
+package com.example.servlet;
+
+import com.example.util.JspHelper;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@WebServlet("/registration")
+public class RegistrationServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("roles", List.of("USER", "ADMIN"));
+        req.setAttribute("genders", List.of("MALE", "FEMALE"));
+
+        req.getRequestDispatcher(JspHelper.getPath("registration"))
+                .forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        var name = req.getParameter("name");
+    }
+}
