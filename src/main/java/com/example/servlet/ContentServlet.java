23fe80ef724d713261268bf71907c5c117ba1d58
RomanKovalenko61 <astek14@mail.ru>
2023-05-05 20:42:24 +0300

    Expression language

--- src/main/java/com/example/servlet/ContentServlet.java
+++ src/main/java/com/example/servlet/ContentServlet.java
@@ -1,5 +1,7 @@
 package com.example.servlet;
 
+import com.example.dto.FlightDTO;
+import com.example.service.FlightService;
 import com.example.util.JspHelper;
 
 import javax.servlet.ServletException;
@@ -8,12 +10,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+
+import static java.util.stream.Collectors.toMap;
 
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
 
+    public static final FlightService flightService = FlightService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<FlightDTO> flightDTOS = flightService.findAll();
+        req.setAttribute("flights", flightDTOS);
+        req.getSession().setAttribute("flightsMap", flightDTOS.stream()
+                .collect(toMap(FlightDTO::getId, FlightDTO::getDescription)));
         req.getRequestDispatcher(JspHelper.getPath("content"))
                 .forward(req, resp);
     }
--- src/main/webapp/WEB-INF/jsp/footer.jsp
+++ src/main/webapp/WEB-INF/jsp/footer.jsp
@@ -1,3 +1,3 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 
-<h1>Footer, Низ</h1>K
+<h1>Footer, Низ</h1>
