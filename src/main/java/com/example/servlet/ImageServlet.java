fba56f249c454e3cac321981d3ef5db898283d54
RomanKovalenko61 <astek14@mail.ru>
2023-05-08 19:17:26 +0300

    Authorization

--- src/main/java/com/example/servlet/ImageServlet.java
+++ src/main/java/com/example/servlet/ImageServlet.java
@@ -1,6 +1,7 @@
 package com.example.servlet;
 
 import com.example.service.ImageService;
+import com.example.util.UrlPath;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,7 +12,7 @@ import lombok.SneakyThrows;
 import java.io.IOException;
 import java.io.InputStream;
 
-@WebServlet("/images/*")
+@WebServlet(UrlPath.IMAGES + "/*")
 public class ImageServlet extends HttpServlet {
 
     private final ImageService imageService = ImageService.getInstance();
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -3,6 +3,7 @@ package com.example.servlet;
 import com.example.dto.UserDTO;
 import com.example.service.UserService;
 import com.example.util.JspHelper;
+import com.example.util.UrlPath;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,7 +13,7 @@ import lombok.SneakyThrows;
 
 import java.io.IOException;
 
-@WebServlet("/login")
+@WebServlet(UrlPath.LOGIN)
 public class LoginServlet extends HttpServlet {
 
     private static final UserService userService = UserService.getInstance();
